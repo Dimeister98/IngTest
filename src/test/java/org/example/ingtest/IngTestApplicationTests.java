6e9d97c2350d689f718e15567f4fb1bd91ef6016
Cosmin Dima <cos_d98@yahoo.com>
2026-07-07 16:07:14 +0300

    First commit

---
+++ src/test/java/org/example/ingtest/IngTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
